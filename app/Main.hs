ba08e858c7c52815c9ea57bcf51a53c936803e31
Valmir Pretto <valmir.pretto@perimeter.company>
2022-07-22 15:51:07 -0300

    Fix typo

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,13 @@ import Syntax.Module (construct, putType, putFunc, putCode, putFuncExport, setSt
 main :: IO ()
 main = write "./result.wasm" $ construct $ do
   type_v <- putType (FuncType (ResultType (Vec [])) (ResultType (Vec [])))
-  type_i_v <- putType (FuncType (ResultType (Vec [ValNumType I32])) (ResultType (Vec [ValNumType I32])))
+  type_i_i <- putType (FuncType (ResultType (Vec [ValNumType I32])) (ResultType (Vec [ValNumType I32])))
   type_v_i <- putType (FuncType (ResultType (Vec [])) (ResultType (Vec [ValNumType I32])))
 
   (funcIdx, _) <- putFunc (Name "test") type_v
   putCode (Code (Func (Vec [Locals 1 (ValNumType I32)]) (Expr [Nop])))
 
-  (_, _) <- putFunc (Name "another_test") type_i_v
+  (_, _) <- putFunc (Name "another_test") type_i_i
   putCode (Code (Func (Vec []) (Expr [I32Const 5])))
 
   (globalIdx, globalSymIdx) <- putGlobalImport (Name "env") (Name "__heap_base") (GlobalType (ValNumType I32) Const)
