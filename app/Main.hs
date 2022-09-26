9120b99c4186531e2ade2b35558f58e73d6b2308
Valmir Pretto <valmirjunior0088@gmail.com>
2022-09-26 13:15:13 -0300

    More complex test

--- app/Main.hs
+++ app/Main.hs
@@ -10,25 +10,34 @@ main = writeModule "./test.wasm" $ runConstruct $ do
   importFunc "env" "dealloc" [ValNumType I32] []
   importGlobal "env" "__stack_pointer" (ValNumType I32) Var
 
-  declareFunc "one_test" [] [ValNumType I32]
-  declareFunc "another_test" [] [ValNumType I32]
-  declareFunc "one_more_test" [] [ValNumType I32]
+  declareFunc "jump_table_test" [ValNumType I32] [ValNumType I32]
+  declareFunc "call_indirect_test" [] [ValNumType I32]
   
   commitFuncRefs
 
-  pushI32FuncRef "another_test"
-  pushCallIndirect [] [ValNumType I32]
-  commitCode
-
-  pushFrame "outer"
-  pushFrame "inner"
+  addParameter "value"
+  pushLocalGet "value"
+  pushFrame "exit"
+  pushFrame "default"
+  pushFrame "case-2"
+  pushFrame "case-1"
+  pushFrame "case-0"
+  pushBrTable ["case-0", "case-1", "case-2"] "default"
+  pushBlock [ValNumType I32] [] -- case-0
+  pushI32Const 5
+  pushBr "exit"
+  pushBlock [ValNumType I32] [] -- case-1
   pushI32Const 6
-  pushBr "outer"
-  pushBlock [] [ValNumType I32]
+  pushBr "exit"
+  pushBlock [ValNumType I32] [] -- case-2
   pushI32Const 7
-  pushBr "outer"
-  pushBlock [] [ValNumType I32]
+  pushBr "exit"
+  pushBlock [ValNumType I32] [] -- default
+  pushUnreachable
+  pushBlock [ValNumType I32] [ValNumType I32] -- exit
   commitCode
 
-  pushGlobalGet "__stack_pointer"
+  pushI32FuncRef "jump_table_test"
+  pushI32Const 1
+  pushCallIndirect [ValNumType I32] [ValNumType I32]
   commitCode
